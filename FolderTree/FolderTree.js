190e774eed48b4dbad9b485928b47e09483b3337
Andrew Pallant <andrewpallant@gmail.com>
2014-03-24 20:21:06 -0400

    Update

--- FolderTree/FolderTree.js
+++ FolderTree/FolderTree.js
@@ -1,48 +1,50 @@
-<script> 
 $(document).ready(function () {
-     $('[id^="subFolder"]').css("display", "none"); 
-     $('.TopFolder').css("display", "block"); 
-     $('[id^="expander"]').click(function () { 
-          var id = this.id.replace("expander", ""); 
-          openHide(id); 
+    $('[id^="subFolder"]').css("display", "none");
+    $('.TopFolder').css("display", "block");
 
-          // Open Parents 
-          $(this).parents("ul").css("display", "block"); 
-    }); 
+    $('[id^="expander"]').click(function () {
+        var id = this.id.replace("expander", "");
 
-    // Open Hide File Nodes 
-	function openHide(id) { 
-	     // Set Icon 
-	     $('[id^="expander"]').attr('src', 'images/closed.png'); 
-	     $('#expander' + id).attr('src', 'images/open.png'); 
+        openHide(id);
 
-	     // Close All - Comment out next 3 lines if you want to open all nodes 
-	     $('[id^="subFolder"]').css("display", "none"); 
-	     $('.TopFolder').css("display", "block"); 
-	     $('#subFolder' + id).css("display", "block"); 
+        // Open Parents
+        $(this).parents("ul").css("display", "block");
+    });
+    
+    // Open Hide File Nodes
+    function openHide(id) {
+        // Set Icon
+        $('[id^="expander"]').attr('src', 'images/closed.png');
+        $('#expander' + id).attr('src', 'images/open.png');
 
-	     // Set Selected 
-	     $('[id^="title"]').removeClass("selected"); 
-	     $('.title').removeClass("selected"); 
-	     $('#title' + id).addClass("selected"); 
-	 } 
+        // Close All - Comment out next 3 lines if you want to open all nodes
+        $('[id^="subFolder"]').css("display", "none");
+        $('.TopFolder').css("display", "block");
+        $('#subFolder' + id).css("display", "block");
 
-	// Click Title Handler - With Sub 
-	$('[id^="title"]').click(function () { 
-	     var id = this.id.replace("title", ""); 
-	     openHide(id); 
+        // Set Selected
+        $('[id^="title"]').removeClass("selected");
+        $('.title').removeClass("selected");
+        $('#title' + id).addClass("selected");
+    }
 
-	     // Open Parents 
-	     $(this).parents("ul").css("display", "block"); 
-	}); 
+    // Click Title Handler - With Sub
+    $('[id^="title"]').click(function () {
+        var id = this.id.replace("title", "");
 
-	// Click Title - No Sub 
-	$('.title').click(function () { 
-	     // Set Selected 
-	     $('[id^="title"]').removeClass("selected"); 
-	     $('.title').removeClass("selected"); 
-	     $(this).addClass("selected"); 
-	});
-}); 
+        openHide(id);
 
-</script>
+        // Open Parents
+        $(this).parents("ul").css("display", "block");
+    });
+    
+    // Click Title - No Sub
+    $('.title').click(function () {
+        
+        // Set Selected
+        $('[id^="title"]').removeClass("selected");
+        $('.title').removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+});
